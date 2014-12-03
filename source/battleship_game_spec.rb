2747f285e0d6c7220dfaf422a45055f1ba1d8448
Megan Gaebler <megan.gaebler@gmail.com>
2014-12-02 23:54:41 -0600

    added a ton of tests for the battleship game. dare I say they're almost comprehensive?

--- source/battleship_game_spec.rb
+++ source/battleship_game_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'battleship.rb'
 
+
 describe 'Game' do
   let(:game){ Game.new }
 
@@ -16,9 +17,42 @@ describe 'Game' do
     expect(game.ships.length).to eq(5)
   end
 
+  describe '#populate_board' do
+    before(:each){ game.populate_board }
+
+    it 'places ships on the board' do
+      expect( game.ships.all? { |ship| ship.placed } ).to be true
+    end
+
+    it 'updates the board with ships' do
+      expect( game.board.flatten ).to include("A", "B", "C", "D", "S")
+    end
+
+    it 'doesn\'t try to place ships on top of one another, populate with an invalid coordinate, or choose an invalid direction' do
+      game2 = Game.new
+      expect{ game2.populate_board }.not_to raise_error
+      game3 = Game.new
+      expect{ game3.populate_board }.not_to raise_error
+      game4 = Game.new
+      expect{ game4.populate_board }.not_to raise_error
+      game5 = Game.new
+      expect{ game5.populate_board }.not_to raise_error
+      game6 = Game.new
+      expect{ game6.populate_board }.not_to raise_error
+      game7 = Game.new
+      expect{ game7.populate_board }.not_to raise_error
+    end
+
+    it 'randomly populates the board' do
+      game2 = Game.new
+      game2.populate_board
+      expect( game.board ).not_to eq( game2.board ) # This has the chance of failing every once in a while because it's random. Run the test again to see if it's failing consistently.
+    end
+  end
+
   describe '#place_ship' do
-    xit 'takes three arguments' do
-      
+    it 'takes three arguments' do
+      expect(Game.instance_method(:place_ship).arity).to eq(3)
     end
 
     it 'raises an InvalidDirectionError if the direction doesn\'t make sense' do
@@ -41,6 +75,62 @@ describe 'Game' do
     it 'doesn\'t allow ships to be placed on top of one another' do
       game.place_ship("Aircraft carrier", "A1", "vertical")
       expect{ game.place_ship("Cruiser", "A2", "horizontal") }.to raise_error( ShipError )
+    end
+  end
+
+  describe '#row' do
+    it 'is a private method' do
+      expect{ game.row(3) }.to raise_error( NoMethodError )
+    end
+
+    it 'returns the correct row from the board' do
+      game.populate_board
+      expect( game.send(:row, 3) ).to eq(game.board[3])
+    end
+  end
+
+  describe '#column' do
+    it 'is a private method' do
+      expect{ game.column(3) }.to raise_error( NoMethodError )
+    end
+
+    it 'returns the correct column from the board' do
+      board_column = game.board.map.each { |row| row[4] }
+      expect( game.send(:column, 4) ).to eq(board_column)
+    end
+  end
+
+  describe '#find_ship' do
+    it 'is a private method' do
+      expect{ game.find_ship("Battleship") }.to raise_error( NoMethodError )
+    end
+
+    it 'returns a ship object' do
+      expect( game.send(:find_ship, "Battleship").is_a? Ship ).to be true
+    end
+
+    it 'returns the correct ship object' do
+      expect( game.send(:find_ship, "Battleship").name ).to eq("Battleship")
+    end
+  end
+
+  describe '#random_coord' do
+    it 'is a private method' do
+      expect{ game.random_coord }.to raise_error( NoMethodError )
+    end
+
+    it 'returns a random lettered coordinate' do
+      expect( game.send(:random_coord) ).to match(/\A[a-jA-J]\d{1,2}\z/) # e.g. "B5"; "C10"
+    end
+  end
+
+  describe '#get_coords' do
+    it 'is a private method' do
+      expect{ game.get_coords("A1") }.to raise_error( NoMethodError )
+    end
+
+    it 'converts letter coordinates into numbered coordinates' do
+      expect( game.send(:get_coords, "A1") ).to eq([0, 0])
     end
   end
 end
