fa392a3cdc628ae29a119d3e4d46c1719f522282
Megan Gaebler <megan.gaebler@gmail.com>
2014-12-04 16:15:07 -0600

    added game views

---
+++ source/game_view.rb
@@ -0,0 +1,42 @@
+require_relative 'game'
+
+class GameView
+  def initialize(game, enemy_game)
+    @game       = game
+    @enemy_game = enemy_game
+    @mock_board = mock_board
+  end
+
+  def render
+    @game.to_s
+  end
+
+  def self.welcome
+<<-welcome
+
+        ******** Welcome to Battleship! *********
+         the game where you don't get in trouble 
+              for shooting at your friends
+welcome
+  end
+
+  def self.prompt
+    "::>"
+  end
+
+  def self.populate_prompt
+    "Start by populating the board."
+  end
+
+  def self.hit(ship_name)
+    "#{ship_name} was hit!"
+  end
+
+  def self.sunk(ship_name)
+    "#{ship_name} has sunk!"
+  end
+
+  def self.miss
+    "Whiff..."
+  end
+end
